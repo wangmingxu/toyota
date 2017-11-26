bbff109c1a01ffeeb23441313e51e21d82b24b2b
王名旭 <1220712534@qq.com>
2017-11-26 20:04:20 +0800

    优化

---
+++ server/server.dev.spa.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const proxyMiddleware = require('proxy-middleware');
+const proxyTable = require('../proxy/prod/proxyTable');
+
+const app = express();
+const { dev } = require('../config/build.config');
+
+// proxy api requests
+Object.keys(proxyTable).forEach((context) => {
+  const options = proxyTable[context];
+  app.use(context, proxyMiddleware(options));
+});
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+// console.log(common.distPath);
+
+app.listen(dev.port, () => {
+  console.log(`App listening on port ${dev.port}!\n`);
+});
