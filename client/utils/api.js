cddd76218a1b94a3fbe88a83305fcb26d685c509
王名旭 <1220712534@qq.com>
2017-12-11 11:29:19 +0800

    增加axios post

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-function apiConfig(map) {
-  const rMap = map;
-
+function apiConfig(rMap) {
   return Object.keys(rMap).reduce((fMap, key) => {
     fMap[key] = (data) => {
       if (typeof rMap[key] === 'string') {
         return axios({ url: rMap[key], params: data });
       }
-      return rMap[key](data);
+      return axios(data);
     };
     return fMap;
   }, {});
@@ -17,18 +15,6 @@ function apiConfig(map) {
 const API = apiConfig({
   getCity: '/hangzhou/singleDog/getCity',
   listLuckyDoy: '/activity/listLuckyDoy',
-  mineSchool: '/oldSchool/mineSchool',
-  loadNotice: '/oldSchool/loadNotice',
-  listFireSchool: '/oldSchool/listFireSchool',
-  searchSchool: '/oldSchool/searchSchool',
-  addSchool: '/oldSchool/addSchool',
-  addQqGroup: '/oldSchool/addQqGroup',
-  schoolInfo: '/oldSchool/schoolInfo',
-  listAudio: '/oldSchool/listAudio',
-  addAudio: '/oldSchool/addAudio',
-  audioInfo: '/oldSchool/audioInfo',
-  loadAudioCount: '/oldSchool/loadAudioCount',
-  mineAudio: '/oldSchool/mineAudio',
   trans: '//oauthbiz.lizhi.fm/checkAppTrans',
 });
 
